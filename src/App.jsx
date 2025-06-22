e378461ccfb4d5a1323fe6194a05bd44cef8f174
Adnan Sami Pavel <adnansamipavel@gmail.com>
2025-06-22 08:11:54 +0600

    style file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+const App = () => {
+  return (
+    <main>
+        <div>
+            <h1>
+                Hello world. This is my portfolio website.....
+            </h1>
+        </div>
+    </main>
+  )
+}
+
+export default App
